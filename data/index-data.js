2171a70cd1c77b2cb298b0a1a5cd1328eb113609
Dang Gia Huy (K18 HL) <huydghe186695@fpt.edu.vn>
2024-09-21 12:55:24 +0700

    first commit

---
+++ data/index-data.js
@@ -0,0 +1,54 @@
+const caurousels = [
+    {
+        id: 1,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+        link: '',
+
+    },
+    {
+        id: 2,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+        link: '',
+    },
+    {
+        id: 3,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+        link: '',
+    }
+
+]
+
+const newly_released_project = [
+    {
+        id: 1,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+
+    },
+    {
+        id: 2,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+    },
+    {
+        id: 3,
+        heading: '',
+        status: '',
+        thumbnail: '',
+        date: '',
+    }
+
+]
